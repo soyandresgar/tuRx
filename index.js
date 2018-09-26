e778f898ded09aa466d999030f5dc38310fc992a
Andrés Gar <andresgarcia@MacBook-Pro-de-Andres-2.local>
2018-09-26 16:22:40 -0500

    side medu actualizado

---
+++ index.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    //x.style.background = "yellow";
+
+    const elem = document.getElementById("opa");
+    elem.style.color="red";
+}
+
+function myFunction2() {
+    //x.style.background = "yellow";
+
+    const elem = document.getElementById("opa");
+    elem.style.color="yellow";
+}
